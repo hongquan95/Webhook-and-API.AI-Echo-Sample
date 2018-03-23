367e566d2bdc19254412617000767c576d237c37
Trân Hồng Quân <hongquan95.tran@gmail.com>
2018-03-23 10:52:00 +0700

    test view 3

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ restService.post("/echo", function(req, res) {
 
 restService.get('/echo', function (req, res) {
   console.log("Nhan mot GET Request ve Homepage");
-  res.send(JSON.stringify(j));
+  res.send(j);
 })
 
 
