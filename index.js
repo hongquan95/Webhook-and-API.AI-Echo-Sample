6656dce96d3b94621e766aded8399379cef0b833
Trân Hồng Quân <hongquan95.tran@gmail.com>
2018-03-23 09:40:27 +0700

    test heroku

--- index.js
+++ index.js
@@ -23,7 +23,8 @@ restService.post("/echo", function(req, res) {
   return res.json({
     speech: speech,
     displayText: speech,
-    source: "webhook-echo-sample"
+    source: "webhook-echo-sample",
+    top: "I am Quan"
   });
 });
 
