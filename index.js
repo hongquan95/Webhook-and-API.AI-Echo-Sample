289dc1920c13fe79de942380ecf4cc65f5afb5a1
Trân Hồng Quân <hongquan95.tran@gmail.com>
2018-03-23 16:50:43 +0700

    test thanh cong get value trang thai

--- index.js
+++ index.js
@@ -29,11 +29,12 @@ restService.post("/echo", function(req, res) {
 
   //--------------------------------
   var speech =
-    req.body.result &&
-    req.body.result.parameters &&
-    req.body.result.parameters.echoText
-      ? req.body.result.parameters.echoText
-      : "Seems like some problem. Speak again.";
+    "Den D2 dang " + j;
+    // req.body.result &&
+    // req.body.result.parameters &&
+    // req.body.result.parameters.echoText
+    //   ? req.body.result.parameters.echoText
+    //   : "Seems like some problem. Speak again.";
   return res.json({
     speech: speech,
     displayText: speech,
