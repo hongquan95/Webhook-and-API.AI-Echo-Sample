e09ebf96d2247553a1a03079ce5fd2d95df61d16
Trân Hồng Quân <hongquan95.tran@gmail.com>
2018-03-24 01:20:13 +0700

    tset callback

--- index.js
+++ index.js
@@ -30,11 +30,11 @@ restService.post("/echo", function(req, res) {
   // console.log("HELLO");
   // // console.log(a);
   // var request = require('request');
-  //   request('http://188.166.206.43/dd6aa1dccaec458d9b8a29f0e8168339/get/D2', function (error, response, body) {
+  //   request('http://188.166.206.43/3dddac1594e74646bde292060be39113/get/D2', function (error, response, body) {
   //   console.log('Status:', response.statusCode);
   //   console.log('Headers:', JSON.stringify(response.headers));
   //   console.log('Response:', body);
-  //   j = body;
+  // //   j = body;
   //   });
     
   axios({
@@ -43,24 +43,26 @@ restService.post("/echo", function(req, res) {
     responseType:'json'
   })
     .then(function(response) {
-      console.log(response.data)
   });
-  var a = axios.interceptors.response.use(function (response) {
-    // Do something with response data
-    return response.data;
+
+  axios.interceptors.response.use(function (response) {
+    console.log("RES",response);
+    j = response;
+    return response;
   }, function (error) {
     // Do something with response error
     return Promise.reject(error);
   });
   
   //--------------------------------
-  
-  var speech = "Den D2 dang " + a;
+  console.log("RESULT = ", j.data)
+  var speech = "Den D2 dang " + j;
     // req.body.result &&
     // req.body.result.parameters &&
     // req.body.result.parameters.echoText
     //   ? req.body.result.parameters.echoText
     //   : "Seems like some problem. Speak again.";
+    
   return res.json({
     speech: speech,
     displayText: speech,
