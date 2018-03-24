0bc3022f3206d3a9c8627602addfb7eb6bb80281
Trân Hồng Quân <hongquan95.tran@gmail.com>
2018-03-25 03:50:21 +0700

    clean code

--- blynkRes.js
+++ blynkRes.js
@@ -1,24 +1,27 @@
 // blynkRes.js
 // ========
+const axios = require("axios");
 module.exports = {
-
-   
-    check_den1_stt: async function () {
-        var request = require('request');
-        var save;
-        try {
-          var result = await request('http://188.166.206.43/3dddac1594e74646bde292060be39113/get/D2', function (error, response, body) {
-          return body;
-          });
-        }
-        catch(error) {
-          console.log("Error!!!");
-          }
-          return result
-        },
-    check_den2_stt: function () {
-      // whatever
-    }
+  test: async function (url) {
+    let json = await axios.get(url);
+    return json;
+  },
+    // check_den1_stt: async function () {
+    //     var request = require('request');
+    //     var save;
+    //     try {
+    //       var result = await request('http://188.166.206.43/3dddac1594e74646bde292060be39113/get/D2', function (error, response, body) {
+    //       return body;
+    //       });
+    //     }
+    //     catch(error) {
+    //       console.log("Error!!!");
+    //       }
+    //       return result
+    //     },
+    // check_den2_stt: function () {
+    //   // whatever
+    // }
   };
   
   var zemba = function () {
--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var blynk = require('./blynkRes');
 const express = require("express");
 const bodyParser = require("body-parser");
-const axios = require("axios");
+// const axios = require("axios");
 //Quan:     3dddac1594e74646bde292060be39113
 //Tu:     dd6aa1dccaec458d9b8a29f0e8168339
 const restService = express();
@@ -14,25 +14,38 @@ restService.use(
     extended: true
   })
 );
-async function getJSONAsync(url) {
-  let json = await axios.get(url);
-  return json;
+// async function getJSONAsync(url) {
+//   let json = await axios.get(url);
+//   return json;
+// }
+function getReturn(res){
+  res.json({
+    speech: speech,
+    displayText: speech,
+    source: "webhook-echo-sample"
+    });
 }
 restService.use(bodyParser.json());
+
 restService.post("/echo", function(req, res) {
   googleReq = req.body.result;
-  var url = 'http://188.166.206.43/3dddac1594e74646bde292060be39113/get/D2';         ;
-
-  getJSONAsync(url).then( function(result) {
+  let url = 'http://188.166.206.43/3dddac1594e74646bde292060be39113/get/D2';         ;
+  blynk.test(url).then( function(result) {
     blynkRes = result.body;
-    console.log(result);
     var speech = "Den D2 dang " + result.data;
-    return res.json({
-      speech: speech,
-      displayText: speech,
-      source: "webhook-echo-sample"
-      });
+    return getReturn(res);
   }); 
+
+  // getJSONAsync(url).then( function(result) {
+  //   blynkRes = result.body;
+  //   console.log(result);
+  //   var speech = "Den D2 dang " + result.data;
+  //   return res.json({
+  //     speech: speech,
+  //     displayText: speech,
+  //     source: "webhook-echo-sample"
+  //     });
+  // }); 
 });
 
 restService.get('/echo', function (req, res) {
