a594222401e31a020bd90a358bce914b22599f1b
Trân Hồng Quân <hongquan95.tran@gmail.com>
2018-03-27 15:30:08 +0700

    Test turn on off all led

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ var state = {on: "0", off: "1", open: "0", close: "1"};
 var room = {bed:"bedroom",kitchen:"kitchen"};
 var sensor = {temp: "temperature", humi: "humidity", gas: "gas"};
 var device = {led: "led", fan: "fan", door: "door", heater: "heater"}
-var pin = {bedroom: "D14", kitchen: "D12",door: "D13", fan: "D0", heater: "D2", temp_humi: "D14", gas: "D5" };
+var pin = {bedroom: "D14", kitchen: "D12",door: "D13", fan: "D0", heater: "D2", temp_humi: "D14", gas: "D5", all_led: "V10" };
 var led
 var blynk_url = 'http://188.166.206.43/dd6aa1dccaec458d9b8a29f0e8168339/'
 var res = {get: "get/", update: "update/"};
@@ -127,7 +127,21 @@ restService.post("/echo", function(g_req, g_res) {
         break;
 
       case 'o.make-all' :
-        getReturn(g_res,'o.make-all');
+        
+        if ( para.parameters.led && para.parameters.state == "" )
+          return getReturn(g_res,"An unknown error");
+        else
+        {
+          // http://blynk-cloud.com/4ae3851817194e2596cf1b7103603ef8/update/D8?value=1
+          let url = blynk_url + res.update + pin.all_led + '?value=' + state[para.parameters.state]
+          console.log("URL = ", url);
+          blynk.write_pin_value_via_get(url).then( function(b_res) {
+            blynkRes = b_res.body;
+            var speech = "All leds are turn " + para.parameters.state;
+            return getReturn(g_res,speech);    
+          });
+          // getReturn(g_res,url);
+        }
         break;
 
       case 'o.check-status-led' :
