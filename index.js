a922da7d18786b3c202e7f22d1fa0d5f54d58130
Trân Hồng Quân <hongquan95.tran@gmail.com>
2018-03-27 15:08:00 +0700

    Finish Test case open-close-door

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ var state = {on: "0", off: "1", open: "0", close: "1"};
 var room = {bed:"bedroom",kitchen:"kitchen"};
 var sensor = {temp: "temperature", humi: "humidity", gas: "gas"};
 var device = {led: "led", fan: "fan", door: "door", heater: "heater"}
-var pin = {bedroom: "D14", kitchen: "D12", fan: "D0", heater: "D2", temp_humi: "D14", gas: "D5" };
+var pin = {bedroom: "D14", kitchen: "D12",door: "D13", fan: "D0", heater: "D2", temp_humi: "D14", gas: "D5" };
 var led
 var blynk_url = 'http://188.166.206.43/dd6aa1dccaec458d9b8a29f0e8168339/'
 var res = {get: "get/", update: "update/"};
@@ -108,12 +108,22 @@ restService.post("/echo", function(g_req, g_res) {
         }
         break;
 
-      // case 'ask-device-state' :
-      //   getReturn(g_res,'ask-device-state');
-      //   break;
-        
       case 'o.open-close-door' :
-        getReturn(g_res,'o.open-close-door');
+        
+        if ( para.parameters.device && para.parameters.make == "" )
+          return getReturn(g_res,"An unknown error");
+        else
+        {
+          // http://blynk-cloud.com/4ae3851817194e2596cf1b7103603ef8/update/D8?value=1
+          let url = blynk_url + res.update + pin[para.parameters.door] + '?value=' + state[para.parameters.make]
+          console.log("URL = ", url);
+          blynk.write_pin_value_via_get(url).then( function(b_res) {
+            blynkRes = b_res.body;
+            var speech = "The " + para.parameters.door + " is " + para.parameters.make;
+            return getReturn(g_res,speech);    
+          });
+          // getReturn(g_res,url);
+        }
         break;
 
       case 'o.make-all' :
